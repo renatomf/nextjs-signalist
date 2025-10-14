f0d45eeef8f77f213fc629e026095a7b7708d491
renatomarf76 <renatomarf76@egmail.com>
2025-10-14 12:23:30 -0300

    7: Stocks Search

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,26 @@
-import Image from "next/image"
-import Link from "next/link"
-import NavItems from "./nav-Items"
-import UserDropdown from "./user-dropdown"
+import Link from "next/link";
+import Image from "next/image";
 
-const Header = ({ user }: { user: User }) => {
-  return (
-    <header className="sticky top-0 header">
-      <div className="container header-wrapper">
-        <Link href="/">
-          <Image 
-            src="/assets/icons/logo.svg"
-            alt="Signalist Logo"
-            width={140}
-            height={32}
-            className="h-8 w-auto cursor-pointer"
-          />
-        </Link>
-        <nav className="hidden sm:block">
-          <NavItems />
-        </nav>
-        <UserDropdown user={user} />
-      </div>
-    </header>
-  )
+import {searchStocks} from "@/lib/actions/finnhub.actions";
+import NavItems from "./nav-Items";
+import UserDropdown from "./user-dropdown";
+
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks();
+
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                </Link>
+                <nav className="hidden sm:block">
+                    <NavItems initialStocks={initialStocks} />
+                </nav>
+
+                <UserDropdown user={user} initialStocks={initialStocks} />
+            </div>
+        </header>
+    )
 }
-
 export default Header
